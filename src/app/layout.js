9881246f2bb88d34328cb382cfa4e4908eb629f9
Martín Castro <castro.cores77@gmail.com>
2024-08-20 15:44:13 +0200

    add logo to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter, Roboto } from "next/font/google";
 import "./globals.css";
 import { Links } from "./components/Links.jsx";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 const roboto = Roboto({ weight: "400", subsets: ["latin"] });
@@ -38,13 +39,20 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
       </head>
       <body className={`${roboto.className} bg-gray-900 text-white`}>
-        <header>
+        <header className="flex flex-col items-center">
           <div className="flex justify-center">
             <h1 className={`${inter.className} text-center text-5xl p-10`}>
               MARTÍN CASTRO{" "}
               <span className="font-bold block">SOFTWARE DEVELOPER</span>
             </h1>
           </div>
+          <Image
+            className="rounded-full"
+            src="/logoPortfolio.webp"
+            alt="Logo de Martín Castro"
+            width={250}
+            height={250}
+          />
           <Links />
         </header>
         {children}
